bc4e108daedba01d0a3726ba60d7c3773219885f
mujahid45 <mujahid.dsi@gmail.com>
2018-02-08 02:18:28 +0530

    change in motd and metadata

--- cookbooks/apache2/metadata.rb
+++ cookbooks/apache2/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mujahid@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures apache2'
 long_description 'Installs/Configures apache2'
-version '0.1.0'
+version '0.2.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
